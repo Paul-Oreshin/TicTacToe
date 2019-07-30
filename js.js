b022aac9cafe7b2a3d010695b3f7095b9ee1e45b
Pavel Oreshin <trixterkorvo@gmail.com>
2019-07-30 22:55:38 +0300

    1

--- js.js
+++ js.js
@@ -1,49 +1,58 @@
-let player1 = prompt("Введите имя игрока: ");
-
-
-let player2 = prompt("Введите имя игрока №2: ");
-
-
+let player1 = prompt("РРјСЏ РїРµСЂРІРѕРіРѕ РёРіСЂРѕРєР°: ");
+let player2 = prompt("РРјСЏ РІС‚РѕСЂРѕРіРѕ РёРіСЂРѕРєР°: ");
 let h1 = document.getElementById("name");
 
-h1.innerHTML = "Ходит игрок: " + player1 + ".";
+h1.innerHTML = "Р’Р°С€ С…РѕРґ: " + player1 + ".";
 
 var table = document.getElementsByClassName("cell");
 
-
 let step = true;
 
-let state = [1, 2, 3,
-    4, 5, 6,
-    7, 8, 9];
+let state = [   1, 2, 3,
+                4, 5, 6,
+                7, 8, 9     ];
 
 
 function check(state, player) {
     if (state[0] == state[1] && state[1] == state[2]) {
         setTimeout(function message() {
-            alert("Победил игрок: " + player)
+            alert("РџРѕР±РµРґРёР» РёРіСЂРѕРє: " + player)
         }, 200);
     }
     if (state[3] == state[4] && state[4] == state[5]) {
-        console.log("Вы победили!");
+        setTimeout(function message() {
+            alert("РџРѕР±РµРґРёР» РёРіСЂРѕРє: " + player)
+        }, 200);
     }
     if (state[6] == state[7] && state[7] == state[8]) {
-        console.log("Вы победили!");
+        setTimeout(function message() {
+            alert("РџРѕР±РµРґРёР» РёРіСЂРѕРє: " + player)
+        }, 200);
     }
     if (state[0] == state[3] && state[3] == state[6]) {
-        console.log("Вы победили!");
+        setTimeout(function message() {
+            alert("РџРѕР±РµРґРёР» РёРіСЂРѕРє: " + player)
+        }, 200);
     }
     if (state[1] == state[4] && state[4] == state[7]) {
-        console.log("Вы победили!");
+        setTimeout(function message() {
+            alert("РџРѕР±РµРґРёР» РёРіСЂРѕРє: " + player)
+        }, 200);
     }
     if (state[2] == state[5] && state[5] == state[8]) {
-        console.log("Вы победили!");
+        setTimeout(function message() {
+            alert("РџРѕР±РµРґРёР» РёРіСЂРѕРє: " + player)
+        }, 200);
     }
     if (state[0] == state[4] && state[4] == state[8]) {
-        console.log("Вы победили!");
+        setTimeout(function message() {
+            alert("РџРѕР±РµРґРёР» РёРіСЂРѕРє: " + player)
+        }, 200);
     }
     if (state[2] == state[4] && state[4] == state[6]) {
-        console.log("Вы победили!");
+        setTimeout(function message() {
+            alert("РџРѕР±РµРґРёР» РёРіСЂРѕРє: " + player)
+        }, 200);
     }
 
 }
@@ -56,7 +65,7 @@ for (let i = 0; i < table.length; i++) {
         if (step) {
             obj.innerHTML = "X";
             state[i] = "X";
-            h1.innerHTML = "Ходит игрок: " + player2 + ".";
+            h1.innerHTML = "Р’Р°С€ С…РѕРґ: " + player2 + ".";
             check(state, player1);
             console.log(state);
             step = !step;
@@ -64,7 +73,7 @@ for (let i = 0; i < table.length; i++) {
         else {
             obj.innerHTML = "O";
             state[i] = "O";
-            h1.innerHTML = "Ходит игрок: " + player1 + ".";
+            h1.innerHTML = "Р’Р°С€ С…РѕРґ: " + player1 + ".";
             check(state, player2);
             console.log(state);
             step = !step;
